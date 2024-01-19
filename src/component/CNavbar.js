fcab787f00ac8e8ea96b961653f1119a2fa2940a
Darshan <darshan.kb@outlook.com>
2024-01-19 10:34:30 +0530

    created custom hooks

--- src/component/CNavbar.js
+++ src/component/CNavbar.js
@@ -1,73 +1,71 @@
-import { React, useEffect, useState } from 'react';
-import {demo} from "../links/demo"
-import '../css/navbar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-const CNavbar = ({balance, theme, headingflag, adminflag}) => {
+import { React, useEffect, useState } from "react";
+import { demo } from "../links/demo";
+import "../css/navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import useGetUser from "./utils/hooks/useGetUser";
+const CNavbar = ({ balance, theme, headingflag, adminflag }) => {
+  let [flag, setFlag] = useState(
+    sessionStorage.getItem("id_token") == null ? false : true
+  );
+  const user = useGetUser();
 
-    let [flag, setFlag] = useState(sessionStorage.getItem('id_token') ==null ? false : true);
-    const [demoStr, setDemoStr] = useState('');
-    const token = sessionStorage.getItem('id_token');
-       //console.log(token);
-        const headers = new Headers();
-        headers.set('Content-type','plain/text');
-        headers.set('Authorization', `Bearer ${token}`);
-        useEffect(() => {
-            const url = demo();
-            console.log(url);
-            fetch(url,{
-                method: 'GET',
-                headers
-            }).then(async (demoData) => {
-                const demo = await demoData.text();
-                //console.log(demo);
-                setDemoStr(demo.split('@')[0]);
-            }).catch((err)=>{
-                console.log(err);
-            });
-        },[]);
-    
-    const logout = () =>{
-        sessionStorage.clear();
-        sessionStorage.removeItem('id_token')
-        window.location.href = "/";
-        setFlag(false);
-    }
+  const logout = () => {
+    sessionStorage.clear();
+    sessionStorage.removeItem("id_token");
+    window.location.href = "/";
+    setFlag(false);
+  };
 
-    return (
-      <div className='slotnavbar'>
-        <div className='heading'>
-          {headingflag && <div className='headingdiv'> Slotmachine Game</div>}
+  return (
+    <div className="slotnavbar">
+      <div className="heading">
+        {headingflag && <div className="headingdiv"> Slotmachine Game</div>}
+      </div>
+      <div className="navtoolbar">
+        <div className="userlabel">{user}</div>
+        <div className="links">
+          <a style={{ color: theme }} href="/">
+            Home
+          </a>
         </div>
-        <div className='navtoolbar'>
-        
-        <div className='userlabel'>
-          {demoStr}
+        <div className="links">
+          {flag === false && (
+            <div style={{ float: "left" }}>
+              <a
+                style={{ color: theme }}
+                href="/login"
+                onClick={() => setFlag(true)}
+              >
+                Login
+              </a>
+            </div>
+          )}
         </div>
-        <div className='links'>
-          <a style={{color:theme}} href="/" >Home</a>
+        <div className="links">
+          {flag === true && (
+            <div style={{ float: "left" }}>
+              <a style={{ color: theme }} href="/slotmachine">
+                Game{" "}
+              </a>
+            </div>
+          )}
         </div>
-        <div className='links'>
-            {
-              flag===false && <div style={{float:"left"}}><a style={{color:theme}} href="/login" onClick={() => setFlag(true)}>Login</a></div>
-            }
+        <div className="links">
+          {flag === true && (
+            <div style={{ float: "left", color: theme }}>
+              <a style={{ color: theme }} href="/claims">
+                Claims{" "}
+              </a>
+            </div>
+          )}
         </div>
-        <div className='links'>
-             {
-                 flag===true && <div style={{float:"left", }}><a style={{color:theme}} href="/slotmachine">Game </a></div>
-             }
-             
-        </div>
-        <div className='links'>
-        {
-                flag===true && <div style={{float:"left", color:theme}}><a style={{color:theme}} href="/claims">Claims </a></div>
-            }
-        </div>
-        <div className='adminDiv'>
-          {adminflag===true && <NavDropdown
+        <div className="adminDiv">
+          {adminflag === true && (
+            <NavDropdown
               id="nav-dropdown-dark-example"
               title="Admin"
               menuVariant="dark"
@@ -75,23 +73,35 @@ const CNavbar = ({balance, theme, headingflag, adminflag}) => {
               <NavDropdown.Item href="/recharge">Recharge</NavDropdown.Item>
               <NavDropdown.Item href="/home">User Details</NavDropdown.Item>
               {/* <NavDropdown.Divider /> */}
-            </NavDropdown>}
+            </NavDropdown>
+          )}
         </div>
-        <div className='links'>
-             {
-              flag===true && <div style={{float:"left", marginLeft:"16px", color:theme, fontWeight:"bold"}}>Balance : {balance}</div>
-             }
+        <div className="links">
+          {flag === true && (
+            <div
+              style={{
+                float: "left",
+                marginLeft: "16px",
+                color: theme,
+                fontWeight: "bold",
+              }}
+            >
+              Balance : {balance}
+            </div>
+          )}
         </div>
-        <div className='logoutbutton'>
-          {
-            flag===true && <div  style={{float:"left"}}><button className='logoutb' onClick={logout}>Logout</button></div>
-          }
-        </div>
+        <div className="logoutbutton">
+          {flag === true && (
+            <div style={{ float: "left" }}>
+              <button className="logoutb" onClick={logout}>
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
-        
-      );
-    
-  }
-   
-  export default CNavbar;
+    </div>
+  );
+};
+
+export default CNavbar;
---
+++ src/component/utils/Url.js
@@ -0,0 +1,1 @@
+export const USER_URL = "http://192.168.1.13:8071/slotmachine/demo";
