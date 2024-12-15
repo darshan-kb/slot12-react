6fc9a51fdcd27fcf6228108c00a865559600444f
Lupin <darshanbehere@gmail.com>
2024-12-15 15:21:14 +0530

    gateway integration

--- src/component/utils/Url.js
+++ src/component/utils/Url.js
@@ -1,5 +1,5 @@
-export const USER_URL = "http://61.2.101.140:8080/slotmachine/demo";
-export const FIX_URL = "http://61.2.101.140:8071/slotmachine/admin/fix";
+export const USER_URL = "http://61.2.101.140:8001/slotmachine/demo";
+export const FIX_URL = "http://61.2.101.140:8001/slotmachine/admin/fix";
 export const SPIN_WEBSOCKET_URL = "http://61.2.101.140:9090/ws";
 export const SPIN_WEBSOCKET_COUNTDOWN_URL = "/countdown/count";
 export const SPIN_WEBSOCKET_RESULT_URL = "/result/data";
@@ -9,4 +9,4 @@ export const SPIN_GET_CLAIM_URL = "http://61.2.101.140:8071/game/api/claims";
 export const SPIN_CLAIM_BET_URL = "http://61.2.101.140:8071/game/api/claims";
 export const SPIN_QUEUE = "http://61.2.101.140:8071/game/queue";
 export const SPIN_QUEUE_WEBSOCKET = "/queue/data";
-export const SIGNUP_URL = "http://61.2.101.140:8080/api/registration";
+export const SIGNUP_URL = "http://61.2.101.140:8001/auth/api/registration";
